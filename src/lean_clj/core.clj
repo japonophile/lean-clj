903920cf3f72e291265a9dec8767092e014c591b
Antoine Choppin <antoine@japonophile.com>
2020-02-10 22:54:34 +0900

    handle file includes

--- src/lean_clj/core.clj
+++ src/lean_clj/core.clj
@@ -13,12 +13,32 @@
           (throw (Exception. "Malformed comment"))))
       text)))
 
+(def read-file)
+
+(defn load-includes
+  "load includes"
+  [text include-tree]
+  (let [m (re-matches #".*(\$\[ (\S+) \$\]).*" text)]
+    (if m
+      (let [filename (get m 2)]
+        (if (some #{filename} include-tree)
+          (throw (Exception. "Include loop detected"))
+          (clojure.string/replace text (get m 1) (read-file filename (conj include-tree filename)))))
+      text)))
+
+(defn read-file
+  "read metamath file"
+  ([filename]
+   (read-file filename [filename]))
+  ([filename include-tree]
+   (load-includes (strip-comments (slurp filename)) include-tree)))
+
 (defn parse-mm
   "some help"
-  [text]
-  (strip-comments text))
+  [filename]
+  (read-file filename))
 
 (defn -main
   "LEAN clojure"
-  []
-  (parse-mm ""))
+  [filename]
+  (parse-mm filename))
--- test/lean_clj/core_test.clj
+++ test/lean_clj/core_test.clj
@@ -10,3 +10,15 @@
     (is (= "abc  def" (strip-comments "abc $( multiline \ncomment $) def")))
     (is (thrown? Exception (strip-comments "abc $( unfinished comment")))
     (is (thrown? Exception (strip-comments "$) abc $( finished comment $)")))))
+
+(deftest test-load-includes
+  (with-redefs [slurp (fn [filename]
+                        (case filename
+                          "xyz.mm" "XYZ"
+                          "xyz-comment.mm" "XYZ $( comment $) ZYX"
+                          "root.mm" "this is meta"))]
+    (testing "load includes"
+      (is (= "abc XYZ def" (load-includes "abc $[ xyz.mm $] def" ["root.mm"])))
+      (is (= "abc XYZ  ZYX def" (load-includes "abc $[ xyz-comment.mm $] def" ["root.mm"]))))
+    (testing "no self inclusion"
+      (is (thrown? Exception (load-includes "abc $[ root.mm $] def" ["root.mm"]))))))
